772d34ef1a1df3b49be06245b12183b129ac83ec
Tim Horton <tmhorton@gmail.com>
2012-05-23 15:08:35 -0700

    Initial commit

---
+++ manifests/base.pp
@@ -0,0 +1,112 @@
+class cluster_pkg {
+
+    user { 'mysql':
+        ensure => present,
+    }
+
+    package { "libaio1":
+        ensure => present
+    }
+
+    exec { "extract-cluster":
+      path => ["/bin", "/usr/bin"],
+      command => "tar -C /usr/local -zxvf /puppet/mysql-cluster-gpl-7.2.6-linux2.6-x86_64.tar.gz",
+      unless => "file /usr/local/mysql 2>/dev/null"
+    } 
+
+    file { "/usr/local/mysql":
+      ensure => link,
+      target => "mysql-cluster-gpl-7.2.6-linux2.6-x86_64"
+    }
+
+    file { "/etc/bash.bashrc":
+      ensure => present,
+      source => "/puppet/bash.bashrc",
+      owner => 'root',
+      group => 'root',
+    }
+}
+
+class cluster {
+  include cluster_pkg
+  exec { "./scripts/mysql_install_db":
+    path => ["/bin", "/usr/bin", "/usr/local/mysql"],
+    cwd => "/usr/local/mysql/",
+    logoutput => true,
+    command => "./scripts/mysql_install_db --user=mysql 1>/tmp/m.out 2>&1",
+    require => File['/usr/local/mysql'],
+    user => 'root',
+    tries => 3, # definitely ghetto
+  } ->
+  exec { "chown_1":
+    path => ["/bin", "/usr/bin"],
+    cwd => "/usr/local/mysql",
+    command => "chown -R root ."
+  } ->
+  exec { "chown_2":
+    path => ["/bin", "/usr/bin"],
+    cwd => "/usr/local/mysql",
+    command => "chown -R mysql data"
+  } ->
+  exec { "chown_3":
+    path => ["/bin", "/usr/bin"],
+    cwd => "/usr/local/mysql",
+    command => "chgrp -R mysql ."
+  } ->
+  file { "/etc/init.d/mysql":
+    ensure => present,
+    source => "/usr/local/mysql/support-files/mysql.server",
+    mode => 744,
+    owner => 'root',
+  } ->
+  file { "/var/log/mysql":
+    ensure => directory,
+    owner => 'root',
+  } ->
+  exec { "update-rc.d":
+    path => ["/bin", "/usr/sbin"],
+    command => "update-rc.d mysql defaults",
+    user => 'root',
+  } ->
+
+    file { "/etc/my.cnf":
+        ensure => present,
+        mode => 0644,
+        owner => 'root',
+        group => 'root',
+        source => "/puppet/my.cnf",
+    } ->
+    exec { "ndbd" :
+      path => ["/usr/local/mysql/bin"],
+      command => "ndbd",
+    } ->
+    exec { "service mysql start":
+      path => ["/bin", "/usr/bin"],
+      command => "service mysql start",
+    }
+}
+
+node 'base_node' {
+    include cluster
+}
+
+node 'node1' inherits base_node { }
+
+node 'node2' inherits base_node { }
+
+node 'mgmt' {
+    include cluster_pkg
+
+    file { "/var/lib/mysql-cluster":
+      ensure => directory,
+      recurse => true,
+    } ->
+    file { "/var/lib/mysql-cluster/config.ini":
+        ensure => present,
+        source => "/puppet/config.ini"
+    } ->
+    exec { "ndb_mgmd" :
+      path => ["/usr/local/mysql/bin"],
+      command => "ndb_mgmd -f /var/lib/mysql-cluster/config.ini --configdir /var/lib/mysql-cluster",
+    }
+}
---
+++ startup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+MYSQL_CLUSTER=manifests/mysql-cluster-gpl-7.2.6-linux2.6-x86_64.tar.gz
+MYSQL_CLUSTER_URL=http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.6-linux2.6-x86_64.tar.gz/from/http://mysql.he.net/
+
+if [ ! $(which vagrant) ]; then
+	echo "vagrant required!"
+	exit 1
+fi
+
+if [ ! $(vagrant box list | grep lucid64) ]; then
+	vagrant box add lucid64 http://files.vagrantup.com/lucid64.box
+fi
+
+if [ ! -e $MYSQL_CLUSTER ]; then
+	wget $MYSQL_CLUSTER_URL -O $MYSQL_CLUSTER
+fi
+
+for d in mgmt node1 node2; do
+	cd $d
+	vagrant up	
+	cd ..
+done
